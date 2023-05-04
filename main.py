dcd6e703de24fbc5b50b31e7f6cfba00f5637abd
Chris <chrissy.drx@gmail.com>
2023-05-04 17:52:59 +0200

    Basic input loop and interaction with OpenAI

--- main.py
+++ main.py
@@ -1,8 +1,25 @@
 from dotenv import load_dotenv
+from langchain.llms import OpenAI
 
 # Initialize env
 load_dotenv()
 
+# Initialize Langchain
+llm = OpenAI(temperature=0.1)
+
+
+def call_open_ai(prompt):
+    print(llm(prompt))
+
 
 if __name__ == '__main__':
-    print('Test')
+    while True:
+        user_input = input("What is your prompt? \n")
+        if user_input == 'exit':
+            print('Process finished')
+            break
+
+        call_open_ai(user_input)
+
+
+
